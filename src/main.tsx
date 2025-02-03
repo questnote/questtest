9acbff7ee5b548601cef1901d9fba95461a4cd1d
Tejas Ranveer <39103436+tejasr7@users.noreply.github.com>
2025-02-03 16:48:34 +0530

    debug main tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Container, Stack } from '@mui/material';
+// import { Button, Container, Stack } from '@mui/material';
 
 
 createRoot(document.getElementById('root')!).render(
